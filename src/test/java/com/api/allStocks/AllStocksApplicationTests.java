73523cb06be1db05b8db3e8c03c5cfd3ff99ce1d
Filipe <filipe.pacheco@meta.com.br>
2023-02-14 20:04:43 -0300

    create aplication

---
+++ src/test/java/com/api/allStocks/AllStocksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.allStocks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllStocksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
