89a82164d6114aedbc1a7e300d48c73e78376808
Filipe <filipe.pacheco@meta.com.br>
2023-02-14 20:34:25 -0300

    feature(commit#003): added SectorsModel

---
+++ src/main/java/com/api/allStocks/models/SectorsModel.java
@@ -0,0 +1,33 @@
+package com.api.allStocks.models;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "sectors")
+public class SectorsModel implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String name;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
